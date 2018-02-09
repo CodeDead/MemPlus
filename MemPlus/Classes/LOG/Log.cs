65bab2d752863fb11e43f1e13a52b01073fcfcdb
CodeDead <admin@codedead.com>
2018-02-09 19:27:19 +0100

    * Added application logs

---
+++ MemPlus/Classes/LOG/Log.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MemPlus.Classes.LOG
+{
+    internal class Log
+    {
+        private readonly DateTime _time;
+        private readonly string _value;
+
+        internal Log(string data)
+        {
+            _time = DateTime.Now;
+            _value = data;
+        }
+
+        internal DateTime GetDate()
+        {
+            return _time;
+        }
+
+        internal string GetValue()
+        {
+            return _value;
+        }
+    }
+}
---
+++ MemPlus/Classes/LOG/LogController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MemPlus.Classes.LOG
+{
+    internal class LogController
+    {
+        private readonly List<Log> _logList;
+
+        internal LogController()
+        {
+            _logList = new List<Log>();
+        }
+
+        internal void AddLog(Log l)
+        {
+            _logList.Add(l);
+        }
+
+        internal void RemoveLog(Log l)
+        {
+            if (_logList.Contains(l))
+            {
+                _logList.Remove(l);
+            }
+            else
+            {
+                throw new ArgumentException("Log could not be found!");
+            }
+        }
+
+        internal void ClearLogs()
+        {
+            _logList.Clear();
+        }
+
+        internal List<Log> GetLogs()
+        {
+            return _logList;
+        }
+    }
+}
