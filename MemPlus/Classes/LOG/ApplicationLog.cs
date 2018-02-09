770e06e0b6dda31892fe9266f6e6fb04c85104f1
CodeDead <admin@codedead.com>
2018-02-09 19:48:35 +0100

    * Improved logging code * Added different log types

---
+++ MemPlus/Classes/LOG/ApplicationLog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MemPlus.Classes.LOG
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// A class that represent a change in the application
+    /// </summary>
+    internal class ApplicationLog : Log
+    {
+        /// <summary>
+        /// Initialize a new ApplicationLog object
+        /// </summary>
+        internal ApplicationLog(string data)
+        {
+            LogType = LogType.Application;
+            Data = data;
+            Time = DateTime.Now;
+        }
+    }
+}
---
+++ MemPlus/Classes/LOG/ILogMethods.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MemPlus.Classes.LOG
+{
+    internal interface ILogMethods
+    {
+        void AddData(string data);
+        string GetData();
+        DateTime GetDate();
+    }
+}
