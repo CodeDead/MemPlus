81a432a6ba35dca622cc1ad390a04e7d08d68571
CodeDead <admin@codedead.com>
2018-02-10 18:22:52 +0100

    * Refactored images * Added code to export logs

---
+++ MemPlus/Classes/LOG/LogExporter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace MemPlus.Classes.LOG
+{
+    /// <summary>
+    /// Interaction logic for exporting logs
+    /// </summary>
+    internal static class LogExporter
+    {
+        internal static void ExportHtml(string path, List<Log> logList)
+        {
+
+        }
+
+        internal static void ExportTxt(string path, List<Log> logList)
+        {
+
+        }
+
+        internal static void ExportCsv(string path, List<Log> logList)
+        {
+
+        }
+
+        internal static void ExportExcel(string path, List<Log> logList)
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// Enumaration containing all the different export types
+    /// </summary>
+    internal enum ExportType
+    {
+        Html,
+        Text,
+        Csv,
+        Excel
+    }
+}
