c591695dbcc87db04a53768c4b83d6e516680574
CodeDead <admin@codedead.com>
2018-02-26 17:52:34 +0100

    * Minor UX improvement to settings window

--- MemPlus/Properties/Settings.Designer.cs
+++ MemPlus/Properties/Settings.Designer.cs
@@ -216,7 +216,7 @@ namespace MemPlus.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1")]
         public int RamMonitorIntervalIndex {
             get {
                 return ((int)(this["RamMonitorIntervalIndex"]));
