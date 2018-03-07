e168b4594ce8f43e4e019eeddcddc4f10edc661d
CodeDead <admin@codedead.com>
2018-03-07 23:40:01 +0100

    * Code refactoring * Fixed possibly throwing wrong win32 error

--- MemPlus/Business/RAM/RamOptimizer.cs
+++ MemPlus/Business/RAM/RamOptimizer.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Security.Principal;
 using MemPlus.Business.LOG;
+using MemPlus.Business.UTILS;
 
 namespace MemPlus.Business.RAM
 {
@@ -90,44 +91,6 @@ namespace MemPlus.Business.RAM
         private readonly LogController _logController;
         #endregion
 
-        #region NativeMethods
-        /// <summary>
-        ///  Retrieves the locally unique identifier (LUID) used on a specified system to locally represent the specified privilege name.
-        /// </summary>
-        /// /// <param name="lpSystemName">A pointer to a null-terminated string that specifies the name of the system on which the privilege name is retrieved. If a null string is specified, the function attempts to find the privilege name on the local system</param>
-        /// <param name="lpName">A pointer to a null-terminated string that specifies the name of the privilege, as defined in the Winnt.h header file. For example, this parameter could specify the constant, SE_SECURITY_NAME, or its corresponding string, "SeSecurityPrivilege"</param>
-        /// <param name="pluid">A pointer to a variable that receives the LUID by which the privilege is known on the system specified by the lpSystemName parameter.</param>
-        /// <returns>If the function succeeds, the function returns nonzero. Otherwise, return value will be zero</returns>
-        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-        private static extern bool LookupPrivilegeValue(string lpSystemName, string lpName, ref long pluid);
-        /// <summary>
-        /// Enables or disables privileges in the specified access token. Enabling or disabling privileges in an access token requires TOKEN_ADJUST_PRIVILEGES access
-        /// </summary>
-        /// <param name="tokenHandle">A handle to the access token that contains the privileges to be modified. The handle must have TOKEN_ADJUST_PRIVILEGES access to the token. If the PreviousState parameter is not NULL, the handle must also have TOKEN_QUERY access</param>
-        /// <param name="disableAllPrivileges">Specifies whether the function disables all of the token's privileges. If this value is TRUE, the function disables all privileges and ignores the NewState parameter. If it is FALSE, the function modifies privileges based on the information pointed to by the NewState parameter</param>
-        /// <param name="newState">A pointer to a TOKEN_PRIVILEGES structure that specifies an array of privileges and their attributes. If the DisableAllPrivileges parameter is FALSE, the AdjustTokenPrivileges function enables, disables, or removes these privileges for the token</param>
-        /// <param name="bufferLength">Specifies the size, in bytes, of the buffer pointed to by the PreviousState parameter. This parameter can be zero if the PreviousState parameter is NULL</param>
-        /// <param name="previousState">A pointer to a buffer that the function fills with a TOKEN_PRIVILEGES structure that contains the previous state of any privileges that the function modifies. That is, if a privilege has been modified by this function, the privilege and its previous state are contained in the TOKEN_PRIVILEGES structure referenced by PreviousState. If the PrivilegeCount member of TOKEN_PRIVILEGES is zero, then no privileges have been changed by this function. This parameter can be NULL</param>
-        /// <param name="returnLength">A pointer to a variable that receives the required size, in bytes, of the buffer pointed to by the PreviousState parameter. This parameter can be NULL if PreviousState is NULL</param>
-        /// <returns>If the function succeeds, the return value is nonzero</returns>
-        [DllImport("advapi32.dll", SetLastError = true)]
-        private static extern bool AdjustTokenPrivileges(IntPtr tokenHandle, bool disableAllPrivileges, ref TokenPrivileges newState, int bufferLength, IntPtr previousState, IntPtr returnLength);
-        /// <summary>Change Windows System parameters</summary>
-        /// <param name="infoClass"></param>
-        /// <param name="info"></param>
-        /// <param name="length">Allocated bytes for the Info block</param>
-        /// <returns>Opposite of boolean.  Zero means success, non-zero means fail and use GetLastError</returns>
-        [DllImport("ntdll.dll")]
-        private static extern uint NtSetSystemInformation(int infoClass, IntPtr info, int length);
-        /// <summary>
-        /// Removes as many pages as possible from the working set of the specified process
-        /// </summary>
-        /// <param name="hwProc">A handle to the process. The handle must have the PROCESS_QUERY_INFORMATION or PROCESS_QUERY_LIMITED_INFORMATION access right and the PROCESS_SET_QUOTA access right</param>
-        /// <returns>If the function succeeds, the return value is nonzero</returns>
-        [DllImport("psapi.dll")]
-        private static extern int EmptyWorkingSet(IntPtr hwProc);
-        #endregion
-
         /// <summary>
         /// Initialize a new RamOptimizer object
         /// </summary>
@@ -158,7 +121,7 @@ namespace MemPlus.Business.RAM
                     {
                         _logController.AddLog(new RamLog("Emptying working set for process: " + process.ProcessName));
                         // Empty the working set of the process
-                        EmptyWorkingSet(process.Handle);
+                        NativeMethods.EmptyWorkingSet(process.Handle);
                         _logController.AddLog(new RamLog("Successfully emptied working set for process " + process.ProcessName));
                     }
                     else
@@ -221,7 +184,9 @@ namespace MemPlus.Business.RAM
                             };
                         systemInfoLength = Marshal.SizeOf(cacheInformation);
                         gcHandle = GCHandle.Alloc(cacheInformation, GCHandleType.Pinned);
-                        ntSetSystemInformationRet = NtSetSystemInformation((int)SystemInformationClass.SystemFileCacheInformation, gcHandle.AddrOfPinnedObject(), systemInfoLength);
+                        ntSetSystemInformationRet = NativeMethods.NtSetSystemInformation((int)SystemInformationClass.SystemFileCacheInformation, gcHandle.AddrOfPinnedObject(), systemInfoLength);
+                        // If value is not equal to zero, things didn't go right :(
+                        if (ntSetSystemInformationRet != 0) throw new Exception("NtSetSystemInformation: ", new Win32Exception(Marshal.GetLastWin32Error()));
                         gcHandle.Free();
 
                         _logController.AddLog(new RamLog("Done clearing 32 bit FileSystem cache information"));
@@ -238,13 +203,13 @@ namespace MemPlus.Business.RAM
                             };
                         systemInfoLength = Marshal.SizeOf(information64Bit);
                         gcHandle = GCHandle.Alloc(information64Bit, GCHandleType.Pinned);
-                        ntSetSystemInformationRet = NtSetSystemInformation((int)SystemInformationClass.SystemFileCacheInformation, gcHandle.AddrOfPinnedObject(), systemInfoLength);
+                        ntSetSystemInformationRet = NativeMethods.NtSetSystemInformation((int)SystemInformationClass.SystemFileCacheInformation, gcHandle.AddrOfPinnedObject(), systemInfoLength);
+                        // If value is not equal to zero, things didn't go right :(
+                        if (ntSetSystemInformationRet != 0) throw new Exception("NtSetSystemInformation: ", new Win32Exception(Marshal.GetLastWin32Error()));
                         gcHandle.Free();
 
                         _logController.AddLog(new RamLog("Done clearing 64 bit FileSystem cache information"));
                     }
-                    // If value is not equal to zero, things didn't go right :(
-                    if (ntSetSystemInformationRet != 0) throw new Exception("NtSetSystemInformation: ", new Win32Exception(Marshal.GetLastWin32Error()));
                 }
 
                 // Clear the standby cache if we have to and if we can also increase the privileges
@@ -255,12 +220,11 @@ namespace MemPlus.Business.RAM
 
                     int systemInfoLength = Marshal.SizeOf(MemoryPurgeStandbyList);
                     GCHandle gcHandle = GCHandle.Alloc(MemoryPurgeStandbyList, GCHandleType.Pinned);
-                    uint ntSetSystemInformationRet = NtSetSystemInformation((int)SystemInformationClass.SystemMemoryListInformation, gcHandle.AddrOfPinnedObject(), systemInfoLength);
+                    uint ntSetSystemInformationRet = NativeMethods.NtSetSystemInformation((int)SystemInformationClass.SystemMemoryListInformation, gcHandle.AddrOfPinnedObject(), systemInfoLength);
+                    if (ntSetSystemInformationRet != 0) throw new Exception("NtSetSystemInformation: ", new Win32Exception(Marshal.GetLastWin32Error()));
                     gcHandle.Free();
 
                     _logController.AddLog(new RamLog("Done clearing standby cache"));
-
-                    if (ntSetSystemInformationRet != 0) throw new Exception("NtSetSystemInformation: ", new Win32Exception(Marshal.GetLastWin32Error()));
                 }
             }
             catch (Exception ex)
@@ -287,16 +251,16 @@ namespace MemPlus.Business.RAM
 
                 _logController.AddLog(new RamLog("Looking up privilage value"));
                 // If we can't look up the privilege value, we can't function properly
-                if (!LookupPrivilegeValue(null, privilegeName, ref newst.Luid)) throw new Exception("LookupPrivilegeValue: ", new Win32Exception(Marshal.GetLastWin32Error()));
+                if (!NativeMethods.LookupPrivilegeValue(null, privilegeName, ref newst.Luid)) throw new Exception("LookupPrivilegeValue: ", new Win32Exception(Marshal.GetLastWin32Error()));
                 _logController.AddLog(new RamLog("Done looking up privilage value"));
 
 
                 _logController.AddLog(new RamLog("Adjusting token privilages"));
                 // Enables or disables privileges in a specified access token
-                int adjustTokenPrivilegesRet = AdjustTokenPrivileges(current.Token, false, ref newst, 0, IntPtr.Zero, IntPtr.Zero) ? 1 : 0;
-                _logController.AddLog(new RamLog("Done adjusting token privilages"));
+                int adjustTokenPrivilegesRet = NativeMethods.AdjustTokenPrivileges(current.Token, false, ref newst, 0, IntPtr.Zero, IntPtr.Zero) ? 1 : 0;
                 // Return value of zero indicates an error
                 if (adjustTokenPrivilegesRet == 0) throw new Exception("AdjustTokenPrivileges: ", new Win32Exception(Marshal.GetLastWin32Error()));
+                _logController.AddLog(new RamLog("Done adjusting token privilages"));
                 return adjustTokenPrivilegesRet != 0;
             }
         }
