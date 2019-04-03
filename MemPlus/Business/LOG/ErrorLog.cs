f2a48896a3d6a3df2b36f739984011c76cf1c9a3
CodeDead <admin@codedead.com>
2019-04-03 19:00:33 +0200

    * Added missing project files

---
+++ MemPlus/Business/LOG/ErrorLog.cs
@@ -0,0 +1,16 @@
+namespace MemPlus.Business.LOG
+{
+    internal sealed class ErrorLog : Log
+    {
+        /// <inheritdoc />
+        /// <summary>
+        /// Initialize a new ErrorLog object
+        /// </summary>
+        /// <param name="data">The data that needs to be added to the Log</param>
+        internal ErrorLog(string data)
+        {
+            LogType = LogType.Error;
+            Data = data;
+        }
+    }
+}
---
+++ MemPlus/Business/LOG/LogType.cs
@@ -0,0 +1,13 @@
+﻿namespace MemPlus.Business.LOG
+{
+    /// <summary>
+    /// An enumeration of all available log types
+    /// </summary>
+    internal enum LogType
+    {
+        Application,
+        Ram,
+        Process,
+        Error
+    }
+}
